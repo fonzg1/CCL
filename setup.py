acdcd9be50c7af3c9973af1fa0dd7931fdd921cd
Nikolai Romashchenko <nikolay.romashchenko@gmail.com>
2020-04-24 13:49:42 +0200

    Fixed setup.py for Visual Studio: look for MSbuild using vswhere.exe

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@
 from setuptools.command.build_py import build_py as _build
 from setuptools.command.develop import develop as _develop
 from setuptools import setup, find_packages
-from subprocess import call
+from subprocess import call, Popen, PIPE
 from io import open
 import numpy
 import os
+import shutil
 import sys
 
 def _check_cmake():
@@ -19,41 +20,74 @@ def _check_mingw():
                            "Make sure mingw-w64 is installed and "
                            " added to the system PATH variable.")
 
+def _check_vswhere():
+    p = Popen(["vswhere.exe", "-h"], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    output, err = p.communicate()
+    if p.returncode != 0:
+        raise RuntimeError("Could not run vswhere.exe. "
+                           "Make sure vswhere is installed (try `conda install vswhere`).")
+
+
+def _find_msbuild():
+    p = Popen(["vswhere.exe", 
+               "-latest",
+               "-requires", "Microsoft.Component.MSBuild",
+               "-find", "MSBuild\**\Bin\MSBuild.exe"], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    output, err = p.communicate()
+    if p.returncode != 0:
+        raise RuntimeError("vswhere.exe could not find msbuild.exe. Make sure Visual Studio is installed.") 
+    return output
+
+
 def _compile_ccl():
-    call(["mkdir", "-p", "build"])
+    # create a build folder if not yet
+    if not os.path.exists("build"):
+        os.makedirs("build")
+
     v = sys.version_info
 
     # Windows build
     if os.name == 'nt':
+
+        # Check if CMake is available
         _check_cmake()
-        #_check_mingw()
 
+        # At the moment pyccl is only compilable by Visual Studio compilers
         use_msvc = True
+        if use_msvc:
 
-        if call(["cmake", 
-                "-H.", "-Bbuild", 
-                #"-G", "MinGW Makefiles",
-                "-DCMAKE_BUILD_TYPE=Release",
-                "-DPYTHON_VERSION=%d.%d.%d" % (
-                    v.major, v.minor, v.micro)]) != 0:
-            raise RuntimeError(
-                "Could not run CMake configuration.")
+            # Check if vswhere.exe is available
+            # It is needed to find where Visual Studio is located
+            _check_vswhere()
 
-        cclpyd_output_dir = "build/pyccl/Release" if use_msvc else "build/pyccl/"
+            # Find MSBuild.exe of the latest Visual Studio available
+            msbuild_path = _find_msbuild().decode('utf-8').strip()
+            print("Found MSBuild.exe:", msbuild_path)
+            
+            # Run CMake to create Visual Studio solution
+            if call(["cmake", 
+                    "-H.", "-Bbuild",
+                    "-DCMAKE_BUILD_TYPE=Release",
+                    "-DPYTHON_VERSION=%d.%d.%d" % (
+                        v.major, v.minor, v.micro)]) != 0:
+                raise RuntimeError("Could not run CMake configuration.")
 
-        if call(["msbuild.exe", "build/pyccl/_ccllib.vcxproj", "-p:Configuration=Release"]) != 0:
-            raise RuntimeError("Could not build CCL.")
+            # Run MSBuild.exe to compile the shared library
+            cclpyd_output_dir = "build\\pyccl\\Release"
+            script_dir = os.path.dirname(os.path.realpath(__file__))
+            if call([msbuild_path, "build\\pyccl\\_ccllib.vcxproj", "-p:Configuration=Release"]) != 0:
+                raise RuntimeError("Could not build CCL.")
 
-        #if call(["mingw32-make.exe", "-Cbuild", "_ccllib"]) != 0:
-        #    raise RuntimeError("Could not build CCL.")
-
-        # Finds the library under its different possible names
-        pyd_file = os.path.join(cclpyd_output_dir, "_ccllib.pyd")
-        if os.path.exists(pyd_file):
-            call(["cp", pyd_file, "pyccl/"])
+            # Find the compiled library and copy it to the target folder
+            pyd_file = os.path.join(cclpyd_output_dir, "_ccllib.pyd")
+            destination = os.path.join("pyccl", os.path.basename(pyd_file))
+            if not os.path.exists(pyd_file) or (not shutil.copyfile(pyd_file, destination)):
+                raise RuntimeError("Could not find wrapper shared library, "
+                                "compilation must have failed.")
         else:
-            raise RuntimeError("Could not find wrapper shared library, "
-                               "compilation must have failed.")
+            
+            #_check_mingw()
+            raise RuntimeError("MinGW is not supported yet.")           
     # Linux build
     else:
         if call(["cmake", "-H.", "-Bbuild",
@@ -72,10 +106,6 @@ def _compile_ccl():
         else:
             raise Exception("Could not find wrapper shared library, "
                             "compilation must have failed.")
-
-    if call(["cp", "build/pyccl/ccllib.py", "pyccl/"]) != 0:
-        raise Exception("Could not find python module, "
-                        "SWIG must have failed.")
 
 class build(_build):
     """Specialized Python source builder."""
